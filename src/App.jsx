917f81311df872a91ad4d8b444ab3367a0286d96
Mangui <antonio.mangado@gmail.com>
2024-04-15 13:10:01 +0200

    ADD/ main routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Dashboard from "./components/Dashboard"
+import GuestList from "./components/GuestList"
+import GuestDetails from "./components/GuestDetails"
+import ConciergeList from "./components/ConciergeList"
+import RoomList from "./components/RoomList"
+import Reviews from "./components/Reviews"
+
 export default function App() {
   
   return (
     <>
-      <h1>App</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/guests" element={<GuestList />} />
+        <Route path="/guests/:id" element={<GuestDetails />} />
+        <Route path="/concierge" element={<ConciergeList />} />
+        <Route path="/rooms" element={<RoomList />} />
+        <Route path="/reviews" element={<Reviews />} />
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
